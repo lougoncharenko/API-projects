99ef5afaf191e4f6e804570099c4f9b1bc30c2a2
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-13 15:56:00 -0500

    Add files via upload

---
+++ cocktail api/script.js
@@ -0,0 +1,32 @@
+//fetch api
+fetch ('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+.then(response => {
+    if(response.ok){
+        return response.json();
+    }else{
+        throw new Error('NETWORK RESPONSE ERROR')
+    }
+})
+.then(data =>{
+    console.log(data)
+    displayDrink(data)
+})
+.catch(error => console.log('Fetch Error:', error));
+
+
+//this function displays the drink on HTML
+function displayDrink(data){
+    //create a varaible for drink object
+    const drink =data.drinks[0];
+    //grab html element div named cocktail
+    const drinkDiv = document.getElementById("cocktail");
+    //grab data of drink name from api object
+    const drinkName=drink.strDrink;
+    //create an h1 element
+    const header= document.createElement('h1');
+    //assign API object information to header
+    header.innerHTML=drinkName;
+    //append it to the HTML div
+    drinkDiv.appendChild(header);
+
+}

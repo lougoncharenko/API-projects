625b8baa41a8cf28d75ecf53d71189ee5886a67e
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-15 16:38:44 -0500

    Add files via upload

--- weather api/app.js
+++ weather api/app.js
@@ -1,16 +1,16 @@
 document.getElementById('searchForm').addEventListener('submit', (e)=>{
-  const searchResults= document.getElementById("searchInput").value;
-  console.log(searchResults)
+  const city= document.getElementById("searchInput").value;
+  console.log(city)
   // console.log(e.value)
-  
+  getWeatherApi(city);
   e.preventDefault();
 })
 
 
 function getWeatherApi(city){
     // this.apikey=d71be756535b836342b5fa5e644f8f8e;
-    
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=28fe7b5f9a78838c639143fc517e4343`)
+    this.city=city;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=28fe7b5f9a78838c639143fc517e4343`)
     .then(response =>{
         if (response.ok) {
             return response.json();
@@ -18,26 +18,33 @@ function getWeatherApi(city){
             throw new Error("NETWORK RESPONSE ERROR");
           }
     })
-    .then(data => {console.log(data)})
+    .then(data => {
+      console.log(data)
+      displayWeather(data);
+    })
     .catch(error => {console.log(error)});
 }
 
 
+function displayWeather(data){
+  const searchResult = document.getElementById("searchResult");
+  //get city name from API & display
+  const cityName = document.getElementById("cityName");
+  cityName.innerHTML=data.name;
 
-//const searchResult = document.getElementById("searchResult");
-// const cityName = document.getElementById("cityName");
-// const temperature = document.getElementById("temperature");
-// const feelsLike = document.getElementById("feelsLike");
-// const humidity = document.getElementById("humidity");
-// const wind = document.getElementById("wind");
+  //get temperature from API and display
+  const temperature = document.getElementById("temperature");
+  temperature.innerHTML=`temperature: ${data.main.temp}°C`;
 
-// cityName.textContent = `${weatherData.cityName}`;
-// temperature.textContent = `${weatherData.temperature} °C`;
-// feelsLike.textContent = `Fells like: ${weatherData.feelsLike} °C`;
-// humidity.textContent = `Humidity: ${weatherData.humidity} %`;
-// wind.textContent = `Wind: ${weatherData.windSpeed} km/h`;
+  //get and display humidity
+  const humidity = document.getElementById("humidity");
+  humidity.innerHTML=`humidity: ${data.main.humidity}%`
+
+  //get and display wind
+  const wind = document.getElementById("wind");
+  wind.innerHTML = `Wind: ${data.windSpeed} km/h`;
+
+}
 
 
-
-getWeatherApi('sarasota');
 
